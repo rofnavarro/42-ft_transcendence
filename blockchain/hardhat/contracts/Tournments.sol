9df96119afbcff6eee8313d03e3323e55015179b
Femoyo <lipemoreira.yorgos@gmail.com>
2024-08-27 20:32:06 -0300

    entendido o problema da blockchain

--- blockchain/hardhat/contracts/Tournments.sol
+++ blockchain/hardhat/contracts/Tournments.sol
@@ -1,5 +1,27 @@
 //SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
-// contract
+contract Tournment
+{
+    string[] public matchData;
+
+	mapping(address => uint256) matchAddress;
+
+	constructor()
+	{
+		matchAddress[msg.sender] = 0;
+	}
+
+    function addMatchData(string memory _matchData) public {
+        matchData.push(_matchData);
+    }
+
+	function getTournments() public view returns (string[] memory, bytes32) {
+        return (matchData, blockhash(block.number - 1));
+    }
+
+	function getTournmentAddress(address _address) public view returns (uint256) {
+		return matchAddress[_address];
+	}
+}
