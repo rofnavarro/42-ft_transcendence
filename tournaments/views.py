bbc19538e01b6195c3c0329442f28b80f102ec0b
Femoyo <lipemoreira.yorgos@gmail.com>
2024-09-28 15:00:39 -0300

    adicionado TODO's para receber ajuda do Ro e usando variaveis de ambiente para controlar o fluxo

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,6 +1,7 @@
 from .models import Tournament
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import QuerySet
+from django.conf import settings
 from users.models import CustomUser
 import datetime
 import json
@@ -9,10 +10,6 @@ import json
 from web3 import Web3
 import json
 
-#TIPS: TX_HASH contem o hash da conta e é usado para instanciar novos contratos
-TX_HASH = ""
-#TIPS: TX_RECEIPT contem o endereço do contrato e é usado interagir com o contrato, aqui esta sendo usado para guardar os "recibos"
-TX_RECEIPT = []
 
 def get_contract():
 	with open('/home/femoyo/Documents/transcendence/blockchain/hardhat/artifacts/contracts/Tournaments.sol/Tournament.json') as f:
@@ -21,38 +18,34 @@ def get_contract():
 
 #TODO: achar uma forma de executar apenas 1x o web3_init
 def web3_init():
-	global TX_HASH
-	global TX_RECEIPT
 	contract = get_contract()
 	web3Instance = Web3(Web3.HTTPProvider('http://localhost:8545/'))
 	Tournament = web3Instance.eth.contract(abi=contract['abi'], bytecode=contract['bytecode'])
 	account = web3Instance.eth.accounts[0]
 	try:
-		TX_HASH = (Tournament.constructor().transact({'from': account}))
-		TX_RECEIPT.append(web3Instance.eth.wait_for_transaction_receipt(TX_HASH))
+		settings.TX_HASH = (Tournament.constructor().transact({'from': account}))
+		settings.TX_RECEIPT.append(web3Instance.eth.wait_for_transaction_receipt(settings.TX_HASH))
 	except Exception as e:
-		print("ITS WORK")
 		pass
 	return (web3Instance)
 
 def deploy_contract(web3):
-	global TX_RECEIPT
 	contract = get_contract()
-	deployed_contract = web3.eth.contract(address=TX_RECEIPT[0].contractAddress, abi=contract['abi'])
+	deployed_contract = web3.eth.contract(address=settings.TX_RECEIPT[0].contractAddress, abi=contract['abi'])
 	return (deployed_contract)
 
 #---------------------------------------------
 
 
 def set_tournament(web3, deployed_contract, object_var):
-	global TX_HASH
+	
 	try:
 		transact = deployed_contract.functions.addMatch(object_var).transact({'from': web3.eth.accounts[0]})
-		TX_RECEIPT.append(transact)
-		receipt = web3.eth.wait_for_transaction_receipt(TX_HASH)
-		TX_RECEIPT.append(receipt)
+		settings.TX_RECEIPT.append(transact)
+		receipt = web3.eth.wait_for_transaction_receipt(settings.TX_HASH)
+		settings.TX_RECEIPT.append(receipt)
 	except Exception as e:
-		print("set Exception",e)
+		print(e)
 	
 	return
 
@@ -67,38 +60,35 @@ def get_tournament(web3, deployed_contract):
 			values.append(json.loads(value_get.replace("'", "\"")))
 			i += 1
 		except Exception as e:
-			print("Get Exception:", i, e)
+			print(e)
 			break
 	return values
 
 #TIPS: função para transformar querySet em json
 def serialize_queryset(queryset: QuerySet) -> dict:
-
     def default(obj):
         if isinstance(obj, datetime.datetime):
             return obj.isoformat()
         raise TypeError(f"Object of type '{obj.__class__.__name__}' is not JSON serializable")
-
     return json.loads(json.dumps(list(queryset), default=default))
 
-
 def tournament(request):
-	global TX_HASH
-	global TX_RECEIPT
-	querySet_obj = Tournament.objects.all().order_by('-start_date')
-	tournaments = serialize_queryset(querySet_obj.values())
-
 	try:
+		querySet_obj = Tournament.objects.all().order_by('-start_date')
+		tournaments = serialize_queryset(querySet_obj.values())
 		#TODO: por as var web3 e deployed_contract em um models para salvar o hash e o address
-		web3 = web3_init()
-		deployed_contract = deploy_contract(web3)
+		if settings.BOOL_WEB3 == False:
+			web3 = web3_init()
+			deployed_contract = deploy_contract(web3)
+			settings.BOOL_WEB3 = True
 
 		#TODO: set precisa ser feito ao final de um torneio
 		for i in range(len(tournaments)):
 			set_tournament(web3, deployed_contract, str(tournaments[i]))
 
-		#TIPS: GET pode ser feito nas visualizações dos torneios desde que haja um TX_HASH
-		values = get_tournament(web3, deployed_contract)
+		#TIPS: GET pode ser feito nas visualizações dos torneios desde que haja um settings.TX_HASH
+		values = get_tournament(web3, deployed_contract)#TODO: set precisa ser feito ao final de um torneio
+		for i in range(len(tournaments)):
 
 		return render(request, 'tournaments/tournaments.html', {'tournament': values})
 	except Exception as e:
